a2b4dfb80e1dc9fcee6a57cec8f545b8e08da81d
aktumar <aktumar.r@gmail.com>
2022-02-18 15:56:32 +0600

    commit

--- RTSP.py
+++ RTSP.py
@@ -16,7 +16,7 @@ config.read("settings.ini")
 
 # SYSTEM = ['151', '155', '157']
 # SYSTEM = ['151_min', '155_min', '157_min']
-SYSTEM = ['155_min']
+SYSTEM = ['155']
 
 for sys in SYSTEM:
     USERNAME = config[sys]["USERNAME"]
@@ -30,7 +30,8 @@ for sys in SYSTEM:
     print(RTSP_URL)
 
     # Open video streaming
-    cap = cv2.VideoCapture(RTSP_URL, apiPreference)
+    # cap = cv2.VideoCapture(RTSP_URL, apiPreference)
+    cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         print('Cannot open RTSP stream')
@@ -40,32 +41,66 @@ for sys in SYSTEM:
         _, frame = cap.read()
 
         # Functions for contours
-        kernel = np.ones((5, 5), np.uint8)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (15, 15), 0, 1, 0, 1)
-        ret, thresh_img = cv2.threshold(blur, 91, 255, cv2.THRESH_BINARY)
-        img_erosion = cv2.erode(thresh_img, kernel, iterations=3)
-        img_dilation = cv2.dilate(img_erosion, kernel, iterations=3)
-
-        contours = cv2.findContours(img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2]
-        for c in contours:
-            cv2.drawContours(frame, [c], -1, (0, 255, 0), 3)
-
-        cv2.imshow(f'{SYSTEM} gray', gray)
-        cv2.imshow(f'{SYSTEM} blur', blur)
-        cv2.imshow(f'{SYSTEM} thresh_img', thresh_img)
-        cv2.imshow(f'{SYSTEM} img_dilation', img_dilation)
-        cv2.imshow(f'{SYSTEM} img_erosion', img_erosion)
+        # kernel = np.ones((5, 5), np.uint8)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # blur = cv2.GaussianBlur(gray, (15, 15), 0, 1, 0, 1)
+        # ret, thresh_img = cv2.threshold(blur, 91, 255, cv2.THRESH_BINARY)
+        # img_erosion = cv2.erode(thresh_img, kernel, iterations=3)
+        # img_dilation = cv2.dilate(img_erosion, kernel, iterations=3)
+        #
+        # contours = cv2.findContours(img_dilation, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)[-2]
+        # for c in contours:
+        #     cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+        #
+        # cv2.imshow(f'{SYSTEM} gray', gray)
+        # cv2.imshow(f'{SYSTEM} blur', blur)
+        # cv2.imshow(f'{SYSTEM} thresh_img', thresh_img)
+        # cv2.imshow(f'{SYSTEM} img_dilation', img_dilation)
+        # cv2.imshow(f'{SYSTEM} img_erosion', img_erosion)
 
         # Delete background
-        lower = np.array([120, 120, 120])
-        upper = np.array([255, 255, 255])
-        thresh = cv2.inRange(frame, lower, upper)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
-        morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-        mask = 255 - morph
-        result = cv2.bitwise_and(frame, frame, mask=mask)
-        cv2.imshow(f'{SYSTEM} result', result)
+        # lower = np.array([170, 170, 170])
+        # upper = np.array([255, 255, 255])
+        # thresh = cv2.inRange(frame, lower, upper)
+        # cv2.imshow("thresh", thresh)
+        # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (20, 20))
+        # morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        # cv2.imshow("morph", morph)
+        # mask = 255 - morph
+        # result = cv2.bitwise_and(frame, frame, mask=mask)
+        # cv2.imshow(f'{SYSTEM} result', result)
+
+        # Delete background better
+        min_area = 0.0005
+        max_area = 0.95
+        mask_color = (100.0, 100.0, 100.0)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow("gray", gray)
+        edges = cv2.Canny(gray, 15, 150)
+        edges = cv2.dilate(edges, None)
+        edges = cv2.erode(edges, None)
+        contour_info = [(c, cv2.contourArea(c),) for c in
+                        cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)[-2]]
+        cv2.imshow("edges", edges)
+        image_area = frame.shape[0] * frame.shape[1]
+        max_area = max_area * image_area
+        min_area = min_area * image_area
+        mask = np.zeros(edges.shape, dtype=np.uint8)
+        for contour in contour_info:
+            print(contour)
+            if contour[1] > min_area and contour[1] < max_area:
+                mask = cv2.fillConvexPoly(mask, contour[0], (255))
+                mask = cv2.dilate(mask, None, iterations=10)
+                mask = cv2.erode(mask, None, iterations=10)
+                mask = cv2.GaussianBlur(mask, (21, 21), 0)
+                cv2.imshow("mask", mask)
+                mask_stack = np.dstack([mask] * 3)
+                mask_stack = mask_stack.astype('float32') / 255.0
+                frame = frame.astype('float32') / 255.0
+                masked = (mask_stack * frame) + ((1-mask_stack) * mask_color)
+                masked = (masked * 255).astype('uint8')
+                cv2.imshow("Foreground", masked)
 
         # Show final result
         cv2.imshow(sys, frame)
